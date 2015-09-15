1e6555a5d13a20766da416e7051d9d972c430233
eahsnoor <chichigirl12@gmail.com>
2015-09-15 11:18:39 -0400

    Name Added

--- Chapter1/Tree/Program.cs
+++ Chapter1/Tree/Program.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Chapter1
+namespace Tree
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
         }
     }
 }
